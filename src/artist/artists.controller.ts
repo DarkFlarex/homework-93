cdc05a70be6ae7c6b9fc2ada0d516a4dc29a705e
DarkFlarex <darkflarexxx@gmail.com>
2024-10-11 16:21:53 +0600

    создаем Dto для артиста. Создаем контроллер для артиста в нем делаем Get запрос, Get по id, delete и post запрос для создания артистов

---
+++ src/artist/artists.controller.ts
@@ -0,0 +1,65 @@
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  Post,
+  UploadedFile,
+  UseInterceptors,
+} from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Artist, ArtistDocument } from '../schemas/artist.schema';
+import { Model } from 'mongoose';
+import { CreateArtistDto } from './create-artist.dto';
+import { FileInterceptor } from '@nestjs/platform-express';
+
+@Controller('artists')
+export class ArtistsController {
+  constructor(
+    @InjectModel(Artist.name)
+    private artistModel: Model<ArtistDocument>,
+  ) {}
+
+  @Get()
+  async getAll() {
+    return this.artistModel.find();
+  }
+
+  @Get(':id')
+  async getOne(@Param('id') id: string) {
+    const artist = await this.artistModel.findById({ _id: id });
+    console.log(artist);
+    if (!artist) {
+      throw new NotFoundException(`Artist with id ${id}`);
+    }
+    return artist;
+  }
+
+  @Delete(':id')
+  async deleteOne(@Param('id') id: string) {
+    const artist = await this.artistModel.findById({ _id: id });
+    if (!artist) {
+      throw new NotFoundException(`Artist with id ${id}`);
+    }
+
+    await this.artistModel.deleteOne({ _id: id });
+
+    return artist;
+  }
+
+  @Post()
+  @UseInterceptors(FileInterceptor('image', { dest: './public/images' }))
+  async create(
+    @Body() categoryDto: CreateArtistDto,
+    @UploadedFile() file: Express.Multer.File,
+  ) {
+    const artist = await this.artistModel.create({
+      name: categoryDto.name,
+      information: categoryDto.information,
+      image: file ? 'images/' + file.filename : null,
+    });
+    return artist;
+  }
+}
