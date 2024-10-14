f74ab5a5544f618a7c70aa017fdf1b4b4725482c
DarkFlarex <darkflarexxx@gmail.com>
2024-10-15 02:31:08 +0600

    homework-94 Добавляем в модуль UserSchema

--- src/app.module.ts
+++ src/app.module.ts
@@ -8,6 +8,8 @@ import { AlbumsController } from './albums/albums.controller';
 import { Album, AlbumSchema } from './schemas/album.schema';
 import { TracksController } from './tracks/tracks.controller';
 import { Track, TrackSchema } from './schemas/track.schema';
+import {User, UserSchema} from "./schemas/user.schema";
+
 
 @Module({
   imports: [
@@ -16,6 +18,7 @@ import { Track, TrackSchema } from './schemas/track.schema';
       { name: Artist.name, schema: ArtistSchema },
       { name: Album.name, schema: AlbumSchema },
       { name: Track.name, schema: TrackSchema },
+      { name: User.name, schema: UserSchema },
     ]),
   ],
   controllers: [
